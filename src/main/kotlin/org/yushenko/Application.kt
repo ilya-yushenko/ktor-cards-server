1f7c3e8eaa35d6ddab0558b107d02050ca785465
Ilya Yushenko <ilya.y@dev.tagsoft.co>
2024-02-07 19:05:11 +0200

    first commit

---
+++ src/main/kotlin/org/yushenko/Application.kt
@@ -0,0 +1,16 @@
+package org.yushenko
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.yushenko.plugins.configureRouting
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+@Suppress("unused")
+fun Application.module() {
+    configureRouting()
+}
